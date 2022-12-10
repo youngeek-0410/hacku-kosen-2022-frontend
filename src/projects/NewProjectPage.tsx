0fe4925a29529eebf8b1ba4cc0455cc8d8ce6d6a
myuto <m.yuto72319@gmail.com>
2022-12-10 16:27:10 +0900

    add tentative UI

--- src/projects/NewProjectPage.tsx
+++ src/projects/NewProjectPage.tsx
@@ -1,10 +1,51 @@
 import React from "react";
 
+import { styled } from "../stitches.config";
+
 export const NewProjectPage: React.FC = () => {
+  const registerProject = () => {
+    alert("registered");
+  };
   return (
-    <>
-      <h2>色紙を作成する</h2>
-      <p></p>
-    </>
+    <Wrapper>
+      <PageTitle>色紙を作成する</PageTitle>
+      <Discription>ここで入力されたお名前はページが公開されるときにも使用されます。</Discription>
+      <InputNameWrapper>
+        <InputName />
+      </InputNameWrapper>
+      <RegisterButtonWrapper>
+        <RegisterProjectButton onClick={registerProject}>色紙を作成する</RegisterProjectButton>
+      </RegisterButtonWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled("div", {
+  marginTop: "32px",
+  textAlign: "center",
+});
+
+const PageTitle = styled("h2", {});
+
+const Discription = styled("p", {
+  marginTop: "16px",
+});
+
+const InputName = styled("input", {
+  borderBottom: "1px solid #000000",
+  borderRight: "none",
+  borderLeft: "none",
+  borderTop: "none",
+});
+
+const RegisterProjectButton = styled("button", {
+  marginTop: "16px",
+});
+
+const InputNameWrapper = styled("div", {
+  marginTop: "16px",
+});
+
+const RegisterButtonWrapper = styled("div", {
+  marginTop: "16px",
+});
