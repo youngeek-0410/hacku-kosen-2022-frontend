dc894a5d1d4373ab9de8d749fa4190a19613f584
igsr5 <igsr5.dev@gmail.com>
2022-12-11 22:06:33 +0900

    Fix

--- src/common/spotifyMusic/utils/authenticate.test.ts
+++ src/common/spotifyMusic/utils/authenticate.test.ts
@@ -3,16 +3,17 @@ import { rest } from "msw";
 
 import { authenticate, authSpotifyApiUrl } from "./authenticate";
 
-const mockServer = setupServer();
-rest.post(authSpotifyApiUrl, (req, res, ctx) => {
-  const resBody = {
-    access_token: "token",
-    token_type: "Bearer",
-    expires_in: 3600,
-  };
+const mockServer = setupServer(
+  rest.post(authSpotifyApiUrl, (req, res, ctx) => {
+    const resBody = {
+      access_token: "token",
+      token_type: "Bearer",
+      expires_in: 3600,
+    };
 
-  return res(ctx.status(200), ctx.json(resBody));
-});
+    return res(ctx.status(200), ctx.json(resBody));
+  })
+);
 
 describe("authenticate spotify api", () => {
   beforeAll(() => {
