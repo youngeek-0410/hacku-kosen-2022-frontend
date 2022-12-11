2cc455290de00c69205e81c1c7da0e362fae2eba
igsr5 <igsr5.dev@gmail.com>
2022-12-11 13:37:31 +0900

    Implement spotifyMusic authenticate

---
+++ src/common/spotifyMusic/authenticate.test.ts
@@ -0,0 +1,31 @@
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+import { authenticate, authUrl } from "./authenticate";
+
+const mockServer = setupServer(
+  rest.post(authUrl, (req, res, ctx) => {
+    const resBody = {
+      access_token: "token",
+      token_type: "Bearer",
+      expires_in: 3600,
+    };
+
+    return res(ctx.status(200), ctx.json(resBody));
+  })
+);
+
+describe("authenticate spotify api", () => {
+  beforeAll(() => {
+    mockServer.listen({
+      onUnhandledRequest: "warn",
+    });
+  });
+  afterAll(() => {
+    mockServer.close();
+  });
+
+  it("should success authendication", async () => {
+    expect(await authenticate()).toBe("token");
+  });
+});
---
+++ src/common/spotifyMusic/authenticate.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export const authUrl = "https://accounts.spotify.com/api/token";
+
+type SpotifyApiAuthRequest = {
+  grant_type: "client_credentials";
+};
+
+type SpotifyApiAuthResponse = {
+  data: {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+  };
+  status: number;
+};
+
+type AccessToken = string;
+
+export const authenticate = async (): Promise<AccessToken> => {
+  const reqBody: SpotifyApiAuthRequest = {
+    grant_type: "client_credentials",
+  };
+  const authOptions: AxiosRequestConfig = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${getClientCredentials()}`,
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  };
+
+  const { data, status } = await axios.post<SpotifyApiAuthRequest, SpotifyApiAuthResponse>(
+    authUrl,
+    reqBody,
+    authOptions
+  );
+  if (status !== 200) throw new Error("failed to authenticate spotify api");
+
+  return data.access_token;
+};
+
+const getClientCredentials = () => {
+  const clientId = process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_ID;
+  const clientSecret = process.env.NEXT_PUBLIC_SPOTIFY_API_CLIENT_SECRET;
+  if (!(clientId && clientSecret)) throw new Error("failed to get spotify api credentials");
+
+  return Buffer.from(clientId + ":" + clientSecret).toString("base64");
+};
