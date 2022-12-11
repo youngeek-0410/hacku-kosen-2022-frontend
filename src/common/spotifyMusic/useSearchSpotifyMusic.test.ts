3711315409830c032b8445dee9b84cd2997787ba
igsr5 <igsr5.dev@gmail.com>
2022-12-11 17:12:22 +0900

    Fix

--- src/common/spotifyMusic/useSearchSpotifyMusic.test.ts
+++ src/common/spotifyMusic/useSearchSpotifyMusic.test.ts
@@ -1,12 +1,9 @@
-// NOTE: デバッグように書いたが、CIで実行できる状態になっていないので、コメントアウトしておく
-
 // import { useSearchSpotifyMusic } from "./useSearchSpotifyMusic";
-//
-// describe("useSearchSpotifyMusic", () => {
-//   it("should return music list", async () => {
-//     expect(await useSearchSpotifyMusic("test")).toBe([]);
-//   });
-// });
-//
-
 export {};
+
+describe("useSearchSpotifyMusic", () => {
+  it("should return music list", async () => {
+    // NOTE: デバッグように書いたが、CIで実行できる状態になっていないので、コメントアウトしておく
+    // expect(await useSearchSpotifyMusic("test")).toBe([]);
+  });
+});
