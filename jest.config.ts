fff4f31513160ce8ea5c4d4c9d2f214b89df5fc0
igsr5 <igsr5.dev@gmail.com>
2022-12-11 13:42:55 +0900

    Fix jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -153,7 +153,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testMatch: ["**/*.test.[jt]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
