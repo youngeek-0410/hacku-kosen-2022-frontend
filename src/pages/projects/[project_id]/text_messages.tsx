8b0c8948710e663d9d2115dfbb7946bcea6ad042
igsr5 <igsr5.dev@gmail.com>
2022-12-13 20:49:59 +0900

    Reafactoring

--- src/pages/projects/[project_id]/text_messages.tsx
+++ src/pages/projects/[project_id]/text_messages.tsx
@@ -1,6 +1,8 @@
+import axios, { AxiosRequestConfig } from "axios";
 import { GetServerSideProps, NextPage } from "next";
 
-import { getTextMessages, TextMessagesPage } from "../../../projects/TextMessagesPage";
+import { backendApiUrl } from "../../../../api";
+import { TextMessagesPage } from "../../../projects/TextMessagesPage";
 import { TextMessage } from "../../../projects/type";
 
 type Props = {
@@ -29,4 +31,29 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
       items,
     },
   };
+};
+
+type GetTextMessagesRequest = {
+  limit?: number;
+};
+
+type GetTextMessagesResponse = {
+  count: number;
+  items: TextMessage[];
+};
+
+const getTextMessages = async (project_id: string): Promise<GetTextMessagesResponse> => {
+  const params: GetTextMessagesRequest = {
+    limit: 100, // NOTE: 100という数字は適当
+  };
+
+  const requestConfig: AxiosRequestConfig<GetTextMessagesRequest> = {
+    url: `${backendApiUrl}/api/projects/${project_id}/text_messages/`,
+    method: "GET",
+    params,
+  };
+
+  const { data, status } = await axios.request<GetTextMessagesResponse>(requestConfig);
+  if (status !== 200) throw new Error("failed to get messages");
+  return data;
 };
--- src/projects/TextMessagesPage.tsx
+++ src/projects/TextMessagesPage.tsx
@@ -1,43 +1,14 @@
-import axios, { AxiosRequestConfig } from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
-import { backendApiUrl } from "../../api";
 import { styled } from "../stitches.config";
 
 import { TextMessage } from "./type";
 
-type GetTextMessagesRequest = {
-  limit: 100;
-};
-
-type GetTextMessagesResponse = {
-  count: number;
-  items: TextMessage[];
-};
-
 type Props = {
   count: number;
   items: TextMessage[];
-};
-
-export const getTextMessages = async (project_id: string): Promise<GetTextMessagesResponse> => {
-  const params: GetTextMessagesRequest = {
-    limit: 100,
-  };
-
-  const requestConfig: AxiosRequestConfig = {
-    url: `${backendApiUrl}/api/projects/${project_id}/text_messages/`,
-    method: "GET",
-    params: {
-      params,
-    },
-  };
-
-  const { data, status } = await axios.request<GetTextMessagesResponse>(requestConfig);
-  if (status !== 200) throw new Error("failed to get messages");
-  return data;
 };
 
 export const TextMessagesPage: React.FC<Props> = (props) => {
