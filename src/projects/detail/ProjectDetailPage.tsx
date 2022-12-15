c5bbdf2d2a36f708c879d926377ec4e7c6c73a5c
myuto <m.yuto72319@gmail.com>
2022-12-15 23:36:13 +0900

    Tweak

--- src/projects/detail/ProjectDetailPage.tsx
+++ src/projects/detail/ProjectDetailPage.tsx
@@ -80,7 +80,7 @@ export const ProjectDetailPage: React.FC<Props> = (props) => {
 const Container = styled("div", {
   width: "80%",
   textAlign: "center",
-  margin: "0 auto",
+  margin: "0 auto 120px",
   position: "relative",
 });
 
