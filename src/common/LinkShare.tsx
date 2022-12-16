3e5cf35c87317b24b6ca0663b2689edf30350fdf
myuto <m.yuto72319@gmail.com>
2022-12-17 08:56:43 +0900

    Fix: hostname

--- src/common/LinkShare.tsx
+++ src/common/LinkShare.tsx
@@ -6,7 +6,7 @@ import { styled } from "../stitches.config";
 
 export const LinkShare: React.FC = () => {
   const router = useRouter();
-  const hostName = "https://hacku-kosen-2022-frontend.vercel.app";
+  const hostName = "https://app.cloveeee.com";
   const url = `${hostName}${router.asPath}`;
   const copyLink = () => {
     navigator.clipboard.writeText(url);
