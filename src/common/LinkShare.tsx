41f35dfeb6b14f5edd06e5730c8c07052567eeec
myuto <m.yuto72319@gmail.com>
2022-12-17 05:41:58 +0900

    get hostname from .env

--- src/common/LinkShare.tsx
+++ src/common/LinkShare.tsx
@@ -6,7 +6,7 @@ import { styled } from "../stitches.config";
 
 export const LinkShare: React.FC = () => {
   const router = useRouter();
-  const hostName = "https://hacku-kosen-2022-frontend.vercel.app";
+  const hostName = process.env.HOSTNAME;
   const url = `${hostName}${router.asPath}`;
   const copyLink = () => {
     navigator.clipboard.writeText(url);
